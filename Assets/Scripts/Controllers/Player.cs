6093d9a5eb0a7e6ccbd5573f4460cdd6665f7fb5
Carlos Solis <csantospablos@gmail.com>
2025-09-15 22:04:13 -0400

    completed the first task

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -10,6 +10,30 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            SpawnBombAtOffset(new Vector3(0, 1, 0));
+        }
 
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            SpawnBombTrail(0.5f, 5);
+        }
+    }
+    private void SpawnBombAtOffset(Vector3 inOffset)
+    {
+        Vector3 spawnPosition = transform.position + inOffset;
+        Instantiate(bombPrefab, spawnPosition, Quaternion.identity);
+    }
+    public void SpawnBombTrail(float inBombSpacing, int inNumberOfBombs)
+    {
+        Vector3 bombTrailDirection = Vector2.down;
+
+        for (int i = 0; i < inNumberOfBombs; i++)
+        {
+            int spaceBehindPlayer = i + 1;
+            Vector3 spawnPosition = transform.position + bombTrailDirection * inBombSpacing * spaceBehindPlayer;
+            Instantiate(bombPrefab, spawnPosition, Quaternion.identity);
+        }
     }
 }

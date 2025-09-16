ef2655f97f7bb90b8812137c2c6a01b672621768
TheAdventuousMiner <csantospablos@gmail.com>
2025-09-16 15:00:39 -0400

    finished first 2 tasks for jounral 3.

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -8,9 +8,35 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
+    [Header("Motion Properties")]
+    public float maxSpeed = 1.0f;
+    public float acceleratonTime = 1.0f;
+
+    private Vector3 velocity = Vector3.zero;
 
     void Update()
     {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float acceleration = maxSpeed / acceleratonTime;
+
+        if (Input.GetKey(KeyCode.LeftArrow))        
+            velocity += acceleration * Time.deltaTime * Vector3.left;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            velocity += acceleration * Time.deltaTime * Vector3.right;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            velocity += acceleration * Time.deltaTime * Vector3.up;
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            velocity += acceleration * Time.deltaTime * Vector3.down;
+
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+        transform.position += velocity * Time.deltaTime;
 
     }
 

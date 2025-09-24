34cb941bfbfc2d1113a147bdfb971a201da5e420
Carlos Solis <csantospablos@gmail.com>
2025-09-23 23:20:56 -0400

    added deceleration

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -11,6 +11,7 @@ public class Player : MonoBehaviour
     [Header("Motion Properties")]
     public float maxSpeed = 1.0f;
     public float acceleratonTime = 1.0f;
+    public float decelerationTime = 1.0f;
 
     private Vector3 velocity = Vector3.zero;
 
@@ -27,21 +28,53 @@ public class Player : MonoBehaviour
     private void PlayerMovement()
     {
         float acceleration = maxSpeed / acceleratonTime;
+        float deceleration = maxSpeed / decelerationTime;
 
-        if (Input.GetKey(KeyCode.LeftArrow))        
+        bool playerAcceleration = false;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
             velocity += acceleration * Time.deltaTime * Vector3.left;
+            playerAcceleration = true;
+        }
 
         if (Input.GetKey(KeyCode.RightArrow))
+        {
             velocity += acceleration * Time.deltaTime * Vector3.right;
+            playerAcceleration = true;
+        }
 
         if (Input.GetKey(KeyCode.UpArrow))
+        {
             velocity += acceleration * Time.deltaTime * Vector3.up;
+            playerAcceleration = true;
+        }
 
         if (Input.GetKey(KeyCode.DownArrow))
+        {
             velocity += acceleration * Time.deltaTime * Vector3.down;
+            playerAcceleration = true;
+        }
 
         velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+
+        if (!playerAcceleration && velocity.magnitude > 0)
+        {
+            Vector3 decelerate = velocity.normalized * deceleration * Time.deltaTime;
+
+            if (decelerate.magnitude > velocity.magnitude)
+            {
+                velocity = Vector3.zero;
+            }
+            else
+            {
+                velocity -= decelerate;
+            }
+            
+        }
+
         transform.position += velocity * Time.deltaTime;
+
 
     }
     private void RadarScan(float radius, int numberOfPoints)

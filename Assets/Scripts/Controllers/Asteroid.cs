f2481c4c34ab4f58d1bd068cbe3c1437588364c7
Carlos Solis <csantospablos@gmail.com>
2025-09-24 00:47:14 -0400

    edited astroid movement script

--- Assets/Scripts/Controllers/Asteroid.cs
+++ Assets/Scripts/Controllers/Asteroid.cs
@@ -8,6 +8,8 @@ public class Asteroid : MonoBehaviour
     public float arrivalDistance;
     public float maxFloatDistance;
 
+    private Vector2 newPoint;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +18,22 @@ public class Asteroid : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        AsteroidMovement();
+    }
+    public void AsteroidMovement()
+    {
+        Vector2 currentPosition = transform.position;
+
+        if (Vector2.Distance(currentPosition, newPoint) < arrivalDistance)
+        {
+            float randomPointX = Random.Range(-maxFloatDistance, maxFloatDistance);
+            float randomPointY = Random.Range(-maxFloatDistance, maxFloatDistance);
+
+            newPoint = currentPosition + new Vector2(randomPointX, randomPointY);
+        }
+
+        Vector2 newDirection = newPoint - currentPosition;
+        Vector2 newPosition = currentPosition + newDirection * moveSpeed * Time.deltaTime;
+        transform.position = newPosition;
     }
 }

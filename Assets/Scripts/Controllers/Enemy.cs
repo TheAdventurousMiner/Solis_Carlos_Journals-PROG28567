4169424c0678dfbf7e3c4b227011ac8763f3c686
Carlos Solis <csantospablos@gmail.com>
2025-09-24 00:25:33 -0400

    edited enemy script

--- Assets/Scripts/Controllers/Enemy.cs
+++ Assets/Scripts/Controllers/Enemy.cs
@@ -3,8 +3,27 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
+    public Transform playerTransform;
+    public float maxSpeed = 1.0f;
+    public float accelerationTime = 1.0f;
+
+    private Vector3 velocity = Vector3.zero;
     private void Update()
     {
+        EnemyMovement();
+    }
+
+    public void EnemyMovement()
+    {
+        Vector3 enemyDirection = playerTransform.position - transform.position;
+
+        float acceleration = maxSpeed / accelerationTime;
+
+        velocity += enemyDirection * acceleration * Time.deltaTime;
+
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+
+        transform.position += velocity * Time.deltaTime;
     }
 
 }

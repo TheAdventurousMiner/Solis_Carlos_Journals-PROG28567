30e33993f8c9155e5b77d41fafc043a5a8709c4f
Carlos Solis <csantospablos@gmail.com>
2025-09-25 04:04:22 -0400

    edited player script

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -15,14 +15,10 @@ public class Player : MonoBehaviour
 
     private Vector3 velocity = Vector3.zero;
 
-    [Header("Radar Properties")]
-    public float radarRadius = 1f;
-    public int numberOfPoints = 6;
 
     void Update()
     {
         PlayerMovement();
-        RadarScan(radarRadius, numberOfPoints);
     }
 
     private void PlayerMovement()
@@ -77,27 +73,5 @@ public class Player : MonoBehaviour
 
 
     }
-    private void RadarScan(float radius, int numberOfPoints)
-    {
-        float angleStep = 360 / numberOfPoints;
-        float radians = angleStep * Mathf.Deg2Rad;
-
-        List<Vector3> points = new List<Vector3>();
-
-        for (int i = 0; i < numberOfPoints; i++)
-        {
-            float adjustment = radians * i;
-            Vector3 point = new Vector3(Mathf.Cos(radians + adjustment), Mathf.Sin(radians + adjustment)) * radius;
-
-            points.Add(point);
-        }
-
-        Vector3 center = transform.position;
-
-        for (int i = 0; i < points.Count - 1; i++)
-        {
-            Debug.DrawLine(center + points[i], center + points[i + 1], Color.green);
-        }
-        Debug.DrawLine(center + points[points.Count - 1], center + points[0], Color.green);
-    }
+    
 }

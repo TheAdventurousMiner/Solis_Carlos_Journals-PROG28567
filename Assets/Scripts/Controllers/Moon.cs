349b9df77426ae2a8f2bc4b7001cc903400d4763
Carlos Solis <csantospablos@gmail.com>
2025-10-02 21:58:36 -0400

    Added power up method to the player script

--- Assets/Scripts/Controllers/Moon.cs
+++ Assets/Scripts/Controllers/Moon.cs
@@ -6,6 +6,12 @@ public class Moon : MonoBehaviour
 {
     public Transform planetTransform;
 
+    [Header("Motion Properties")]
+    public float orbitRadius;
+    public float orbitSpeed;
+    public Transform moonTransform;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +21,11 @@ public class Moon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        OrbitalMotion(orbitRadius, orbitSpeed, planetTransform);
+    }
 
+    public void OrbitalMotion(float radius, float speed, Transform target)
+    {
+        speed += Time.deltaTime;
     }
 }
--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
+    public GameObject powerupPrefab;
+
     [Header("Motion Properties")]
     public float maxSpeed = 1.0f;
     public float acceleratonTime = 1.0f;
@@ -19,10 +21,19 @@ public class Player : MonoBehaviour
     public int numberOfPoints = 6;
     private Color radarColour;
 
+    [Header("Powerup Properties")]
+    public float powerupRadius;
+    public int amountOfPowerups;
+
     void Update()
     {
         PlayerMovement();
         RadarScan(radarRadius, numberOfPoints);
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            SpawnPowerups(powerupRadius, amountOfPowerups);
+        }
     }
 
     private void PlayerMovement()
@@ -78,6 +89,23 @@ public class Player : MonoBehaviour
             Debug.DrawLine(center + points[i], center + points[i + 1], radarColour);
         }
         Debug.DrawLine(center + points[points.Count - 1], center + points[0], radarColour);
+    }
+
+    public void SpawnPowerups(float radius, int numberOfPowerups)
+    {
+        float angleStep = 360f / numberOfPowerups;
+        float radians = angleStep * Mathf.Deg2Rad;
+
+        Vector3 center = transform.position;
+
+        for (int i = 0;i < numberOfPowerups;i++)
+        {
+            float adjustment = radians * i;
+            Vector3 direction = new Vector3(Mathf.Cos(radians + adjustment), Mathf.Sin(radians + adjustment)) * radius;
+            Vector3 spawnPosition = center + direction * radius;
+
+            Instantiate(powerupPrefab, spawnPosition, Quaternion.identity);
+        }
     }
 
 }

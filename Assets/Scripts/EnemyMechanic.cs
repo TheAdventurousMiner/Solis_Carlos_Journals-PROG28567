f0bd957fe5c2d812325fd20fc1f3386c4e70f080
TheAdventurousMiner <csantospablos@gmail.com>
2025-10-10 15:43:36 -0400

    added enemy mechanic

--- Assets/Scripts/EnemyMechanic.cs
+++ Assets/Scripts/EnemyMechanic.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class EnemyMechanic : MonoBehaviour
 {
+    public Transform player;
+    public Transform blackhole;
+    public float speed = 1f;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {

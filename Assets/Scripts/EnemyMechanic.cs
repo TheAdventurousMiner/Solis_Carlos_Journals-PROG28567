1d3bfc9f2b6f1bab0be4a0c6fad943e4b14e2844
Carlos Solis <csantospablos@gmail.com>
2025-10-10 22:12:57 -0400

    Created experiment for vector mechanic

--- Assets/Scripts/EnemyMechanic.cs
+++ Assets/Scripts/EnemyMechanic.cs
@@ -5,7 +5,9 @@ public class EnemyMechanic : MonoBehaviour
     public Transform player;
     public Transform blackhole;
     public float speed = 1f;
+    public float blackholeRadius = 5f;
 
+    private Transform currentTarget;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,6 +18,22 @@ public class EnemyMechanic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float distanceToBlackHole = Vector2.Distance(transform.position, blackhole.position);
+
+        if (distanceToBlackHole < blackholeRadius )
+        {
+            currentTarget = blackhole;
+        }
+        else
+        {
+            currentTarget = player;
+        }
+
+        Vector2 direction = currentTarget.position - transform.position;
+
+        Vector2 movement = direction * speed * Time.deltaTime;
+
+        transform.position += (Vector3)movement; 
     }
+
 }

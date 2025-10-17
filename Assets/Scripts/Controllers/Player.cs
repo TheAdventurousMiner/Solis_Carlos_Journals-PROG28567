d6ccbf68c46e62851ce5f256d50f25832c43b5ea
TheAdventurousMiner <csantospablos@gmail.com>
2025-10-17 13:02:19 -0400

    added controls for player, including the velocity, acceleration, and deceleration.

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -9,9 +9,61 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
+    [Header("Motion Properties")]
+    public float maxSpeed = 1.0f;
+    public float accelerationTime, decelerationTime;
+    private float acceleration, deceleration;
+    private Vector3 velocity;
+
+    private void Start()
+    {
+        acceleration = maxSpeed/accelerationTime;
+        deceleration = maxSpeed/decelerationTime;
+    }
+
     void Update()
     {
+        Vector2 playerInput = Vector2.zero;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            playerInput += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            playerInput += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerInput += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            playerInput += Vector2.right;
+        }
 
+        if (playerInput.magnitude > 0)
+        {
+            velocity += (Vector3)playerInput.normalized * acceleration * Time.deltaTime;
+
+            if (velocity.magnitude > maxSpeed)
+            {
+                velocity = velocity.normalized * maxSpeed;
+            }
+        }
+        else
+        {
+            Vector3 changeInVelocity = velocity.normalized * deceleration * Time.deltaTime;
+            if(changeInVelocity.magnitude > velocity.magnitude)
+            {
+                velocity = Vector3.zero;
+            }
+            else
+            {
+                velocity -= changeInVelocity;
+            }
+        }
+
+        transform.position += velocity * Time.deltaTime;
     }
 
 }

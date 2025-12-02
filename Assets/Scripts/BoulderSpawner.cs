302ffcbf30df80bcbdada0cffec2122e6b303fc1
TheAdventuousMiner <csantospablos@gmail.com>
2025-12-02 14:46:48 -0500

    added boulder

---
+++ Assets/Scripts/BoulderSpawner.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class BoulderSpawner : MonoBehaviour
+{
+    public GameObject boulderPrefab;
+    public float boulderSpawnDelay = 4f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    IEnumerator Start()
+    {
+        GameObject boulder = Instantiate(boulderPrefab, transform.position, Quaternion.identity, transform);
+
+        Rigidbody2D body2D = boulder.GetComponent<Rigidbody2D>();
+        body2D.AddForce(Random.insideUnitCircle, ForceMode2D.Impulse);
+
+        yield return new WaitForSeconds(boulderSpawnDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ParallaxLayerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ParallaxLayerController : MonoBehaviour
+{
+    [SerializeField] private Camera viewCamera;
+    [SerializeField] private float cameraDeltaScalar;
+
+    private Vector3 cameraStartPos;
+    private Vector3 layerStartPos;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        cameraStartPos = viewCamera.transform.position;
+        layerStartPos = transform.position;
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        Vector3 cameraDelta = viewCamera.transform.position - cameraStartPos;
+
+        float deltaX = cameraDelta.x * cameraDeltaScalar;
+        float deltaY = cameraDelta.y * cameraDeltaScalar;
+
+        transform.position = layerStartPos + new Vector3(deltaX, deltaY);
+    }
+}
